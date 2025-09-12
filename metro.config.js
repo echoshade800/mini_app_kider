9115a8250875665e500984ccc02bdc388b8f7967
echoshade800 <echoshade800@gmail.com>
2025-09-12 19:31:36 +0800

    修复Expo localhost连接问题

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,12 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Configure server settings
+config.server = {
+  port: 8081,
+  host: '0.0.0.0',
+};
+
+// Configure resolver for better asset handling
+config.resolver.platforms = ['ios', 'android', 'native', 'web'];
 module.exports = config;

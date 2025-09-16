00f969c8ea4551dd796066aa090afbe1f3ea9847
echoshade800 <echoshade800@gmail.com>
2025-09-16 19:33:38 +0800

    Fix import.meta error in Metro config

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,13 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Configure transformer to handle import.meta for web
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve('metro-react-native-babel-transformer'),
+  unstable_allowRequireContext: true,
+};
+
 // Configure server settings
 config.server = {
   port: 8081,
@@ -10,4 +17,5 @@ config.server = {
 
 // Configure resolver for better asset handling
 config.resolver.platforms = ['ios', 'android', 'native', 'web'];
+
 module.exports = config;

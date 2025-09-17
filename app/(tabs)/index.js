3ced4642177be350c0c8bef91704295170b365b8
echoshade800 <echoshade800@gmail.com>
2025-09-17 17:43:15 +0800

    更新主图URL

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -5,7 +5,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import StorageUtils from '../utils/StorageUtils';
 import { STAGE_NAMES } from '../utils/stageNames';
 
-const HERO_URL = 'https://dzdbhsix5ppsc.cloudfront.net/monster/20250917-165940.jpeg';
+const HERO_URL = 'https://dzdbhsix5ppsc.cloudfront.net/monster/20250917-173743.jpeg';
 
 export default function Home() {
   const [latestLevelName, setLatestLevelName] = useState('新手上路');
